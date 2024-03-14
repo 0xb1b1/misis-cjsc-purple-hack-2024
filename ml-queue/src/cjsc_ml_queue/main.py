a69473a65ced8d692387db3783dc1bc1eb355a40
0xb1b1 <voxelflee@protonmail.com>
2024-03-14 03:40:19 +0300

    util(ml-queue): convert to rest (fastapi)

--- ml-queue/src/cjsc_ml_queue/main.py
+++ ml-queue/src/cjsc_ml_queue/main.py
@@ -1,14 +1,18 @@
+#!/usr/bin/env python3
+import uvicorn
 import redis
 import sys
-import asyncio
-import socketio
-import json
 from fastapi import FastAPI
+from cjsc_ml_queue.models import Message, MessageRequestConfig, MessageRequest
 
 app = FastAPI()
 
 # Connect to the Redis server
-redis_conn = redis.Redis(host='localhost', port=6379, db=0)
+redis_conn = redis.Redis(
+    host='localhost',
+    port=6379,
+    db=0
+)
 
 
 # Function to push an item to the queue
@@ -16,63 +20,26 @@ def push_to_queue(redis_conn, item):
     redis_conn.lpush('requests', item)
 
 
-async def run():
-    # asyncio
-    async with socketio.AsyncSimpleClient() as sio:
-        await sio.connect("http://127.0.0.1:8080", namespace="/ml")
-        
-        # Authenticate with JWT TOKEN
-        await sio.emit("auth", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0Ijp7ImlkIjoxLCJlbWFpbCI6InUwQGV4YW1wbGUuY29tIiwicm9sZSI6InVzZXIifSwidHlwZSI6ImFjY2VzcyIsImV4cCI6MTcxMjg2ODk2MSwiaWF0IjoxNzEwMjc2OTYxLCJqdGkiOiJhNDQ5MjUwNi02OGVhLTQ4YTMtOTlmMy0wMmZhZWYwZmY0NTAifQ.rQYU4ImwpbqA9-lT4vwj_W_lxmsmWuHlIJArZvp5vc0")
+@app.post("/query_ml")
+async def query_ml(mr: MessageRequest):
+    # Push the request to the queue
+    push_to_queue(redis_conn, mr.json())
+    return {"status": "ok"}
 
-        # Wait for the server to respond
-        event = await sio.receive()
-        print(event)
 
-        # Get chats list
-        await sio.emit("chats_list")
-        chat_list = await sio.receive()
-        print(chat_list)
+@app.post("/ml_answer")
+async def ml_answer(m: Message):
+    ...  # Send POST to backend with the message
 
-        # Listen for chat messages
-        send_msg_counter = 0
-        await sio.emit("chats_listen")
-        
-        while True:
-            if send_msg_counter % 10 == 0:
-                print(f"Sending message to 4: send_msg_counter: {send_msg_counter}")
-                await sio.emit("chat_send", {"message": {"to": 4, "content": f"send_msg_counter: {send_msg_counter}"}})
-            
-            try:
-                event = await sio.receive(timeout=3)
-                """
-                [
-                ml_listen: str,
-                payload: dict = {
-                    "message": {"id": int, "content": str}, 
-                    "config": {"allow_faq": bool}
-                    }
-                ]
-                """
 
-                item = json.dumps(event[1])
-                push_to_queue(item)
-
-                # ЗДЕСЬ НАДО ДОБАВЛЯТЬ В БД
-
-            except Exception as e:
-                print(e)
-                send_msg_counter += 1
-                continue
-                
-            if event[0] == "chat_message" and "message" in event[1]:
-                print(event)
-                
-            if event[0] == "heartbeat":
-                print("Received heartbeat")
-                
-            send_msg_counter += 1
+def run():
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8081,
+    )
 
 
 if __name__ == "__main__":
-    asyncio.run(run())
+    run()
     sys.exit(0)
